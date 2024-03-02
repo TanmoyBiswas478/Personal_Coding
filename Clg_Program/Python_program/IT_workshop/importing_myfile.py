119985bc6171a70369817478360f74abab18292d
Tanmoy Biswas <tanmoybiswas478@gmail.com>
2024-03-02 22:52:15 +0530

    Update importing_myfile.py

--- Clg_Program/Python_program/IT_workshop/importing_myfile.py
+++ Clg_Program/Python_program/IT_workshop/importing_myfile.py
@@ -1,4 +1,5 @@
-from def_all import age,add,subtract,multiply,divide,circle_area,circle_parameter,celsius_to_fahrenheit,fahrenheit_to_celsius,choose,age
+#importing one file into another file
+from def_all import age,add,subtract,multiply,divide,circle_area,circle_parameter,celsius_to_fahrenheit,fahrenheit_to_celsius,age
 
 print(add(1,2))
 print(subtract(4,2))
@@ -7,8 +8,5 @@ print(divide(1,2))
 print(circle_area(7))
 print(circle_parameter(7))
 print(celsius_to_fahrenheit(42))
-print(fahrenheit_to_celsius(12))
-print(choose(7))
+print(fahrenheit_to_celsius(72))
 print(age(2007))
-
-
