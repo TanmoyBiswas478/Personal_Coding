a3e198bc928e0b8d84f049459ed3af0128d8b8a7
Tanmoy Biswas <tanmoybiswas478@gmail.com>
2024-03-02 19:58:49 +0530

    Python_Programming

---
+++ Python/practice.py
@@ -0,0 +1,29 @@
+# # def greater(a,b):
+# #     if(a>b):
+# #         print(a,"is greater than",b)
+# #     elif(b>a):
+# #         print(b,"is greater than",a)
+# #     else:
+# #         print(a ,"or",b,"is equals to ",b,"or",a)
+# # a=494
+# # b=494
+# # greater(a,b)
+# def name(name1, name2,name3,name4):
+#     print("Hello,", name1, name2,name3,name4)
+# name("Sam", "Wilson",4,9.3)
+
+# # def abantika(love):
+# #     love="abantika"
+# #     print(love)
+# # abantika(love)
+
+# import time
+# t = time.strftime('%D           %H:%M:%S')
+# hour=int(time.strftime('%H'))
+# print(hour)
+# if(hour>=0 and hour<12):
+#   print("Good Morning Sir!")
+# elif(hour>=12 and hour<17):
+#   print("Good Afternoon Sir!")
+# elif(hour>=17 and hour<0):
+#   print("Good Night Sir!")
