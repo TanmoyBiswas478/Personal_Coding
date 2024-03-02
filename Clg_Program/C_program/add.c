bdfa45e9d241478f9a7133e60e22ab433b9503e1
Tanmoy Biswas <tanmoybiswas478@gmail.com>
2024-03-02 20:20:02 +0530

    Clg_Programming

---
+++ Clg_Program/C_program/add.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,c;
+    printf("Enter a number : ",a);
+    scanf("%d",&a);
+    printf("Enter a number : ",b);
+    scanf("%d",&b);
+    printf("The result is : %d",c=a+b);
+    scanf("%d",&c);
+    return 0;
+}
---
+++ Clg_Program/C_program/multiply.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    float a,b,c;
+    printf("Enter a number : ");
+    scanf("%f",&a);
+    printf("Enter a number : ");
+    scanf("%f",&b);
+    printf("The result is : %0.3f",c=a*b);
+    return 0;
+}
